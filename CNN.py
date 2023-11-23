d988f00b2887319d0db03f806dd1a0a014b2d6f5
Konstantin Pechatkin <phetryok@gmail.com>
2023-11-23 17:35:39 +0000

    Лабораторная работа 4

---
+++ CNN.py
@@ -0,0 +1,22 @@
+import torch.nn as nn
+
+# Определение архитектуры нейросети
+class ConvNet(nn.Module):
+    def __init__(self, num_classes=3):
+        super(ConvNet, self).__init__()
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1)
+        self.relu = nn.ReLU()
+        self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
+        self.conv2 = nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1)
+        self.fc1 = nn.Linear(128 * 32 * 32, num_classes)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.relu(x)
+        x = self.pool(x)
+        x = self.conv2(x)
+        x = self.relu(x)
+        x = self.pool(x)
+        x = x.view(x.size(0), -1)
+        x = self.fc1(x)
+        return x
